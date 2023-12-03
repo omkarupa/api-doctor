c2a695214e99bb1e30f09b434749d091233ceda5
Omkar Upadhyay <omkarmh009@gmail.com>
2023-12-03 11:15:45 +0800

    Initial Doctor API commit

---
+++ api-doctor/src/main/java/com/po_solutions/apidoctor/ApiDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.po_solutions.apidoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDoctorApplication.class, args);
+	}
+
+}
