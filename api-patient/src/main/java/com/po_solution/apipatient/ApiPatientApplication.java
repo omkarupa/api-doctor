76f200ae56dd09af561af8c6d6eec0fcbc302068
omakarupa <omkarmh009@gmail.com>
2023-12-03 12:10:10 +0800

    Patient initial API commit

---
+++ api-patient/src/main/java/com/po_solution/apipatient/ApiPatientApplication.java
@@ -0,0 +1,13 @@
+package com.po_solution.apipatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPatientApplication.class, args);
+	}
+
+}
---
+++ api-patient/src/test/java/com/po_solution/apipatient/ApiPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.po_solution.apipatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
