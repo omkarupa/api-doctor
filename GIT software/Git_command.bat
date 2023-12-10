d0251492452522f0f95b5b6f256d7152de067f7d
Omkar Upadhyay <omkarmh009@gmail.com>
2023-12-10 18:19:44 +0800

    File merge

---
+++ GIT software/Git_command.bat
@@ -0,0 +1,24 @@
+cd "api-doctor"
+
+git checkout omkar-branch-1
+
+git add .
+
+git commit -m ""
+
+git push origin omkar-branch-1
+
+git checkout main
+
+git pull origin main
+
+git merge omkar-branch-1
+
+git push origin main
+
+git checkout omkar-branch-1
+
+git merge main
+
+git push origin omkar-branch-1
+
