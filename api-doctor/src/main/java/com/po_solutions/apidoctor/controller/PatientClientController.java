7582c7668b470e0af62673b8245b34a9424927c0
omakarupa <omkarmh009@gmail.com>
2023-12-10 17:47:52 +0800

    API call for patient client from Doctor API

--- api-doctor/src/main/java/com/po_solutions/apidoctor/controller/PatientClientController.java
+++ api-doctor/src/main/java/com/po_solutions/apidoctor/controller/PatientClientController.java
@@ -1,10 +1,14 @@
 package com.po_solutions.apidoctor.controller;
 
+import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.po_solutions.apidoctor.client.entity.Patient;
 
 @RestController
 @RequestMapping("api-doctor")
@@ -16,6 +20,16 @@ public class PatientClientController {
 	@GetMapping("patient-client/welcome")
 	public ResponseEntity<String> weilcomePatient(){
 		return client.patientURL();
+	}
+	
+	@GetMapping("Patient-client/")
+	public ResponseEntity<Patient> getPatientDetails(@PathVariable("doctorId") String doctorId){
+		ResponseEntity<Patient> patient=  client.getPatientByPatientId(doctorId);
+		patient.getBody();
+		if(null!=patient)
+			return patient;
+		return new ResponseEntity<Patient>(org.springframework.http.HttpStatus.BAD_REQUEST);
+		
 	}
 	
 }
