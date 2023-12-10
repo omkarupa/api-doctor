f9dcdc43721b20661f0c27e2c331edc474fecab8
Omkar Upadhyay <omkarmh009@gmail.com>
2023-12-10 19:04:40 +0800

    File merge

--- api-patient/src/main/java/com/po_solution/apipatient/controllers/PatientController.java
+++ api-patient/src/main/java/com/po_solution/apipatient/controllers/PatientController.java
@@ -43,7 +43,7 @@ public class PatientController {
 	 }
 		 
 	 @GetMapping("/patient/{patient-id}")
-	 public ResponseEntity<Patient> getPatientByPatientId(@PathVariable String patientIdString)
+	 public ResponseEntity<Patient> getPatientByPatientId(@PathVariable("patient-id") String patientIdString)
 	 {
 		 if(patientIdString != null)
 		 {
