9b3dd5bb0ca9297b1b99ebd0829c5149f3f53154
Omkar Upadhyay <Omkar.Upadhyay@C2LBIZMUM.COM>
2023-12-11 10:15:26 +0800

    API project for Medical Questions

---
+++ api-medical-questions/src/main/java/com/po_solutions/apimedicalquestions/ApiMedicalQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.po_solutions.apimedicalquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicalQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicalQuestionsApplication.class, args);
+	}
+
+}
---
+++ api-medical-questions/src/test/java/com/po_solutions/apimedicalquestions/ApiMedicalQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.po_solutions.apimedicalquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedicalQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
